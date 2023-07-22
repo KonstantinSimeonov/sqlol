0524cb9ac53812cc37b2031f96d25ea290f9dfc5
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-07-22 13:47:22 +0300

    add bytea stuff for sheetz and gigglez

--- ids/generate-id.sql
+++ ids/generate-id.sql
@@ -1,23 +1,25 @@
 
+drop table if exists posts;
 drop function if exists generate_id;
 -- TODO: figure out bytea, char(32) seems terrible compared to uuid
-create function generate_id() returns char(32) as
+create function generate_id() returns bytea as
 $$
     declare ts text;
     declare rnd text;
 begin
     ts := extract(epoch from current_timestamp)::int::text;
-    rnd := substring(md5(random()::text), 1, 32 - length(ts) - 1);
-    return (ts || '-' || rnd)::char(32);
+    rnd := substring(md5(random()::text), 1, 36 - length(ts) - 1);
+    return (ts || '-' || rnd)::bytea;
 end;
 $$ language plpgsql;
 
-drop table if exists posts;
 create table posts (
-    id char(32) primary key default(generate_id()),
+    id bytea primary key default(generate_id()),
     content text not null,
     title text not null
 );
+
+SET bytea_output = 'hex';
 
 insert into posts
     (content, title)
@@ -44,11 +46,9 @@ values
     ('haha', 'test'),
     ('test', 'omri');
 
-select * from posts;
+select convert_from(id, 'UTF8'), * from posts order by id desc;
 
-select * from posts order by id desc;
-
-select * from posts where id > (select id from posts offset 4 limit 1) order by id;
+select convert_from(id, 'UTF8'), * from posts where id > (select id from posts offset 4 limit 1) order by id;
 
 insert into posts
     (id, content, title)
@@ -57,4 +57,4 @@ values
     ('1700031995-dd63014a94326d14d95de', 'kir4o', 'kekw');
 
 -- 9 posts expected
-select * from posts where id between '1690021996' and '1690031994';
+select convert_from(id, 'UTF8'), * from posts where id between '1690021996' and '1690031994';
